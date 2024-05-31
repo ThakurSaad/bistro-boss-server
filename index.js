86458e064a31d425b6aca3918f2a75f436f7a438
ThakurSaad <thakursaad613@gmail.com>
2024-05-31 19:58:33 +0600

    cwd-8 module-65

--- index.js
+++ index.js
@@ -22,14 +22,25 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+    const menuCollection = client.db("bistroDb").collection("menu");
+    const reviewCollection = client.db("bistroDb").collection("reviews");
     console.log("Connected to MongoDB!");
+
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  console.log("object");
   res.send("Buy Food");
 });
 
